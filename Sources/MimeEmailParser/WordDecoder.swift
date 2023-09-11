104e1240faa281f7947155d37401d4d6684140bc
Joshua Halickman <jhalickman@gmail.com>
2023-09-11 11:45:16 -0400

    Remove bad comments

--- Sources/MimeEmailParser/WordDecoder.swift
+++ Sources/MimeEmailParser/WordDecoder.swift
@@ -153,14 +153,6 @@ public class WordDecoder {
                 return String.Encoding.unicode
         }
    
-        /*let cfe = CFStringConvertIANACharSetNameToEncoding(charset as CFString)
-        if cfe != kCFStringEncodingInvalidId {
-            let se = CFStringConvertEncodingToNSStringEncoding(cfe)
-            let sse = String.Encoding(rawValue: se)
-            return sse
-        } else {
-            return String.Encoding.unicode
-        }
     }
     
     fileprivate func convert(charset:String, content:String) throws -> String {
